cccb96e693a3dba6558892728b32f9a80fa2832a
Team ECC <teamecc18@gmail.com>
2019-01-02 12:49:01 +0530

    Changed youtube frame size.

--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 $('#play-video').on('click', function (e) {
     e.preventDefault();
     $('#video-overlay').addClass('open');
-    $("#video-overlay").append('<iframe width="560" height="315" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
+    $("#video-overlay").append('<iframe width="1000" height="600" src="https://www.youtube.com/embed/ngElkyQ6Rhs" frameborder="0" allowfullscreen></iframe>');
 });
 
 $('.video-overlay, .video-overlay-close').on('click', function (e) {
